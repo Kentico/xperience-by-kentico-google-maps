f2887974e42fc96a1cd9577ba5daaf311a0c78f5
Jan Minář <janm@kentico.com>
2024-04-04 15:31:57 +0200

    Add basic form component

---
+++ Kentico.Xperience.GoogleMaps/Models/FormComponents/AddressFormComponentProperties.cs
@@ -0,0 +1,27 @@
+﻿using CMS.DataEngine;
+using Kentico.Forms.Web.Mvc;
+using Kentico.Xperience.Admin.Base.FormAnnotations;
+
+namespace Kentico.Xperience.GoogleMaps.Models.FormComponents
+{
+    public class AddressFormComponentProperties : FormComponentProperties<string>
+    {
+        /// <summary>
+        /// Gets or sets the default value of the form component and underlying field.
+        /// </summary>
+        [TextInputComponent(Label = "{$kentico.formbuilder.defaultvalue$}", Order = EditingComponentOrder.DEFAULT_VALUE)]
+        public override string DefaultValue { get; set; } = string.Empty;
+
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TextInputProperties"/> class.
+        /// </summary>
+        /// <remarks>
+        /// The constructor initializes the base class to data type <see cref="FieldDataType.Text"/> and size 500.
+        /// </remarks>
+        public AddressFormComponentProperties()
+            : base(FieldDataType.Text, 500)
+        {
+        }
+    }
+}
