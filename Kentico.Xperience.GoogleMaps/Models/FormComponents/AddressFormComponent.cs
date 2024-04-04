1bc2c39008bc760476110a9546968faa019b8e32
Jan Minář <janm@kentico.com>
2024-04-04 15:31:57 +0200

    Add basic form component

---
+++ Kentico.Xperience.GoogleMaps/Models/FormComponents/AddressFormComponent.cs
@@ -0,0 +1,34 @@
+﻿using Kentico.Forms.Web.Mvc;
+using Kentico.Xperience.GoogleMaps.Models.FormComponents;
+
+[assembly: RegisterFormComponent(AddressFormComponent.IDENTIFIER,
+                                 typeof(AddressFormComponent),
+                                 "Address",
+                                 IconClass = "icon-home")]
+
+namespace Kentico.Xperience.GoogleMaps.Models.FormComponents
+{
+    public class AddressFormComponent : FormComponent<AddressFormComponentProperties, string>
+    {
+        public const string IDENTIFIER = "AddressFormComponent";
+
+
+        /// <summary>
+        /// Represents the input value in the resulting HTML.
+        /// </summary>
+        [BindableProperty]
+        public string Value { get; set; } = string.Empty;
+
+
+        /// <summary>
+        /// Gets the <see cref="Value"/>.
+        /// </summary>
+        public override string GetValue() => Value;
+
+
+        /// <summary>
+        /// Sets the <see cref="Value"/>.
+        /// </summary>
+        public override void SetValue(string value) => Value = value;
+    }
+}
