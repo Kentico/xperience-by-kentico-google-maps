a6c618200b8ab1805e4f9d100a6885ba009cb293
Jan Minář <janm@kentico.com>
2024-04-05 15:33:09 +0200

    Add options and startup extensions

---
+++ Kentico.Xperience.GoogleMaps/Options/GoogleMapsOptions.cs
@@ -0,0 +1,13 @@
+﻿namespace Kentico.Xperience.RepoTemplate.Options
+{
+    /// <summary>
+    /// Represents Google Maps options.
+    /// </summary>
+    public sealed class GoogleMapsOptions
+    {
+        /// <summary>
+        /// API Key used to send requests.
+        /// </summary>
+        public string? APIKey { get; set; }
+    }
+}
