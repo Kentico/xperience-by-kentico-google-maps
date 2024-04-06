f7ff937177f38f31073716caceed02d1f11d5572
Jan Minář <janm@kentico.com>
2024-04-06 11:49:28 +0200

    Add SetupExtensions unit tests

---
+++ Kentico.Xperience.GoogleMaps.Tests/Extensions/StartupExtensionsTests.cs
@@ -0,0 +1,74 @@
+﻿using System.Net.Http.Headers;
+using CMS.Tests;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+using NUnit.Framework;
+
+namespace Kentico.Xperience.GoogleMaps.Tests
+{
+    /// <summary>
+    /// Tests for <see cref="StartupExtensions"/> class.
+    /// </summary>
+    public class StartupExtensionsTests
+    {
+        [TestFixture]
+        public class AddGoogleMapsTests : UnitTests
+        {
+            private const string APIKEY = "APIKey";
+
+            private IServiceCollection services;
+            private IConfiguration configuration;
+
+
+            [SetUp]
+            public void Setup()
+            {
+                services = new ServiceCollection();
+            }
+
+
+            [Test]
+            public void AddGoogleMaps_ValidOptions_SetupOptionsAddHttpClient()
+            {
+                configuration = new ConfigurationBuilder()
+                    .AddInMemoryCollection(new Dictionary<string, string>
+                    {
+                        { $"{GoogleMapsConstants.SECTION_KEY}:{nameof(GoogleMapsOptions.APIKey)}", APIKEY }
+                    })
+                    .Build();
+
+                services.AddGoogleMaps(configuration);
+
+                var serviceProvider = services.BuildServiceProvider();
+                var googleMapsOptions = serviceProvider.GetRequiredService<IOptions<GoogleMapsOptions>>();
+                var httpClientFactory = serviceProvider.GetService<IHttpClientFactory>();
+                var httpClient = httpClientFactory.CreateClient(GoogleMapsConstants.CLIENT_NAME);
+
+                Assert.Multiple(() =>
+                {
+                    Assert.That(googleMapsOptions, Is.Not.Null);
+                    Assert.That(googleMapsOptions.Value.APIKey, Is.EqualTo(APIKEY));
+                    Assert.That(httpClient.DefaultRequestHeaders.Accept, Is.InstanceOf<HttpHeaderValueCollection<MediaTypeWithQualityHeaderValue>>());
+                    Assert.That(httpClient.DefaultRequestHeaders.Accept.Contains(new MediaTypeWithQualityHeaderValue("application/json")));
+                });
+            }
+
+
+            [TestCase(null, TestName = "AddGoogleMaps_APIKeyNull_ThrowsException")]
+            [TestCase("", TestName = "AddGoogleMaps_APIKeyEmpty_ThrowsException")]
+            public void AddGoogleMaps(string apiKey)
+            {
+                configuration = new ConfigurationBuilder()
+                    .AddInMemoryCollection(new Dictionary<string, string>
+                    {
+                        { $"{GoogleMapsConstants.SECTION_KEY}:{nameof(GoogleMapsOptions.APIKey)}", apiKey },
+                    })
+                    .Build();
+
+                Assert.That(() => services.AddGoogleMaps(configuration), Throws.InvalidOperationException);
+            }
+        }
+    }
+}
