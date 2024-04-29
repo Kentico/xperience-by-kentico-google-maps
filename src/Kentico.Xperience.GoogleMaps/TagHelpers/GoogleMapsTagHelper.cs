791e8d5340edef2ecb47ea4f5b652cad802485a6
Jan Minář <janm@kentico.com>
2024-04-29 15:05:57 +0200

    Add missing docs

--- src/Kentico.Xperience.GoogleMaps/TagHelpers/GoogleMapsTagHelper.cs
+++ src/Kentico.Xperience.GoogleMaps/TagHelpers/GoogleMapsTagHelper.cs
@@ -3,17 +3,29 @@ using Microsoft.AspNetCore.Razor.TagHelpers;
 
 namespace Kentico.Xperience.GoogleMaps
 {
+    /// <summary>
+    /// Tag helper for rendering Google Maps scripts and address form component tags.
+    /// </summary>
     public class GoogleMapsTagHelper : TagHelper
     {
         private readonly IGoogleMapsScriptsRenderer googleMapsScriptsRenderer;
 
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GoogleMapsTagHelper"/> class.
+        /// </summary>
+        /// <param name="googleMapsScriptsRenderer">The Google Maps scripts renderer.</param>
         public GoogleMapsTagHelper(IGoogleMapsScriptsRenderer googleMapsScriptsRenderer)
         {
             this.googleMapsScriptsRenderer = googleMapsScriptsRenderer;
         }
 
 
+        /// <summary>
+        /// Processes the Google Maps tag helper.
+        /// </summary>
+        /// <param name="context">The tag helper context.</param>
+        /// <param name="output">The tag helper output.</param>
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
             if (context == null)
